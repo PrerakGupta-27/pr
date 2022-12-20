48e2f4a5b17615567168c1e33420ffb3dcbcfc3e
PrerakGupta-27 <105355874+PrerakGupta-27@users.noreply.github.com>
2022-12-20 20:59:20 +0530

    Update kernelcode.c

--- kernelcode.c
+++ kernelcode.c
@@ -3,40 +3,27 @@
 #include <linux/kernel.h>
 #include <linux/sched.h>
 #include <linux/pid.h>
-
-struct task_struct *get_task_struct_by_pid(pid_t pid) {
-    struct task_struct *task;
-
-    rcu_read_lock();
-    task = find_task_by_vpid(pid);
-    if (task) {
-        get_task_struct(task);
-    }
-    rcu_read_unlock();
-
-    return task;
-}
+#include <linux/sched/signal.h>
 
 void print_process_info(struct task_struct *task) {
     printk(KERN_INFO "Process name: %s\n", task->comm);
     printk(KERN_INFO "Process ID: %d\n", task->pid);
-    printk(KERN_INFO "Process state: %ld\n", task->state);
-    printk(KERN_INFO "Process priority: %d\n", task->prio);
+    printk(KERN_INFO "User ID: %d\n", task->cred->uid.val);
+    printk(KERN_INFO "Group ID: %d\n", task->group_leader->pid);
 }
 
-static int __init my_module_init(void) {
+int my_module_init(void) {
     pid_t pid = 1; // Replace 1 with the desired PID
-    struct task_struct *task = get_task_struct_by_pid(pid);
-    if (task) {
-        print_process_info(task);
-        put_task_struct(task);
-    } else {
-        printk(KERN_ERR "Failed to get task struct for PID %d\n", pid);
-    }
+    struct task_struct *task;
+    for_each_process(task){
+        if (task->pid==pid) {
+            print_process_info(task);
+        }
+    } 
     return 0;
 }
 
-static void __exit my_module_exit(void) {
+void my_module_exit(void) {
     printk(KERN_INFO "Exiting kernel module\n");
 }
 
@@ -45,6 +32,3 @@ module_exit(my_module_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Kernel module to print process information");
-
-make
-sudo insmod my_module.ko
