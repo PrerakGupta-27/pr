e1394a083fc705fff232e9d5ce35b7fc129baaad
PrerakGupta-27 <105355874+PrerakGupta-27@users.noreply.github.com>
2022-12-21 17:52:36 +0530

    Update kernelcode.c

--- kernelcode.c
+++ kernelcode.c
@@ -1,34 +1,60 @@
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/sched.h>
-#include <linux/pid.h>
-#include <linux/sched/signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-void print_process_info(struct task_struct *task) {
-    printk(KERN_INFO "Process name: %s\n", task->comm);
-    printk(KERN_INFO "Process ID: %d\n", task->pid);
-    printk(KERN_INFO "User ID: %d\n", task->cred->uid.val);
-    printk(KERN_INFO "Group ID: %d\n", task->group_leader->pid);
+#define NUM_PHILOSOPHERS 5
+
+void pickup_forks(int philosopher_id) {
+// Pick up the left fork
+printf("Philosopher %d: Picking up left fork\n", philosopher_id);
+
+// Pick up the right fork
+printf("Philosopher %d: Picking up right fork\n", philosopher_id);
 }
 
-int my_module_init(void) {
-    pid_t pid = 1; // Replace 1 with the desired PID
-    struct task_struct *task;
-    for_each_process(task){
-        if (task->pid==pid) {
-            print_process_info(task);
-        }
-    } 
-    return 0;
+void return_forks(int philosopher_id) {
+// Put down the left fork
+printf("Philosopher %d: Putting down left fork\n", philosopher_id);
+
+// Put down the right fork
+printf("Philosopher %d: Putting down right fork\n", philosopher_id);
 }
 
-void my_module_exit(void) {
-    printk(KERN_INFO "Exiting kernel module\n");
+void eat(int philosopher_id) {
+printf("Philosopher %d: Eating\n", philosopher_id);
+sleep(1);
 }
 
-module_init(my_module_init);
-module_exit(my_module_exit);
+void think(int philosopher_id) {
+printf("Philosopher %d: Thinking\n", philosopher_id);
+sleep(1);
+}
 
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Kernel module to print process information");
+void* philosopher_thread(void* philosopher_id_ptr) {
+int philosopher_id = ((int) philosopher_id_ptr);
+
+while (1) {
+think(philosopher_id);
+pickup_forks(philosopher_id);
+eat(philosopher_id);
+return_forks(philosopher_id);
+}
+
+return NULL;
+}
+
+int main() {
+pthread_t philosophers[NUM_PHILOSOPHERS];
+int philosopher_ids[NUM_PHILOSOPHERS];
+
+for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+philosopher_ids[i] = i;
+pthread_create(&philosophers[i], NULL, philosopher_thread, &philosopher_ids[i]);
+}
+
+for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+pthread_join(philosophers[i], NULL);
+}
+
+return 0;
+}
