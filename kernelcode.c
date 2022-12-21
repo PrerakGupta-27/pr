2c05a8055f8ed40908e81a91ced099a6f44424e5
PrerakGupta-27 <105355874+PrerakGupta-27@users.noreply.github.com>
2022-12-21 21:04:08 +0530

    Update kernelcode.c

--- kernelcode.c
+++ kernelcode.c
@@ -1,105 +1,118 @@
-#include<stdio.h>
 #include<stdlib.h>
+#include<stdio.h>
 #include<string.h>
 #include<sys/socket.h>
 #include<sys/un.h>
 #include<unistd.h>
 #include<time.h>
+#include<stdint.h>
 
-#define SOCKET_NAME "mySocket.socket"
+#define SOCKET_NAME "Aryan_Dhull.socket"
 #define BUFFER_SIZE 10
 
-void getRandStr(char randstrs[][12]) {        
+void getRandStr(char randstrs[][10]) {        
     srand (time(NULL));                            
     for (int j = 0; j <= 50; j++) {
         randstrs[j][0] = j;
     }
     
     for(int i = 0; i <= 50; i++) {
-		for(int j = 1; j <= 10; j++) {
-			randstrs[i][j] = rand() % 26 + 65;
-		}
-	}
+        for(int j = 1; j <= 8; j++) {
+            randstrs[i][j] = rand() % 26 + 65;
+        }
+    }
 }
 
-int main(int argc, char* argv[]) {
-	struct sockaddr_un addr;
-	int ret;
-	int data_socket;
-	char buffer[BUFFER_SIZE];
+void printer(char randStr[50][10]){
+        for(int i=1;i<=50;i++){
+        for(int j=0;j<10;j++){
+            if(j==0){
+                printf("%d ",randStr[i][j]);
+            }
+            else{
+                printf("%c",randStr[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
 
-	//Creating data socket
-	data_socket = socket(AF_UNIX, SOCK_SEQPACKET, 0);
-	if(data_socket == -1) {
-		perror("Couldn't create socket");
-		exit(EXIT_FAILURE);
-	}	
+int main(int argc,char* argv[]){
+    struct timespec s,e;
+    clock_gettime(CLOCK_MONOTONIC,&s);
+    struct sockaddr_un soc;
+    int connector;
+    int dsoc;
+    char buffer[10];
+    dsoc=socket(AF_UNIX,SOCK_SEQPACKET,0);
+    if(dsoc==-1){
+        perror("Couldn't create socket");
+        exit(EXIT_FAILURE);
+    }
 
-	memset(&addr, 0, sizeof(addr));
+    memset(&soc,0,sizeof(soc));
+    
+    soc.sun_family= AF_UNIX;
+    strncpy(soc.sun_path,SOCKET_NAME,sizeof(soc.sun_path)-1);
+    connector=connect(dsoc,(const struct sockaddr *)&soc,sizeof(soc));
 
-	//For local connections
-	addr.sun_family = AF_UNIX;
-	strncpy(addr.sun_path, SOCKET_NAME, sizeof(addr.sun_path) - 1);
-	ret = connect(data_socket, (const struct sockaddr *) &addr, sizeof(addr));
+    if(connector==-1){
+        perror("Server is down\n");
+        exit(EXIT_FAILURE);
+    }
 
-	if(ret == -1) {
-		perror("Server is down\n");
-		exit(EXIT_FAILURE);
-	}	
+    char strg[51][12]={{0}};
+    getRandStr(strg);
 
-	//Writing data
-	char randStr[51][12] = {{0}};
-	getRandStr(randStr);
-	
-	for(int i = 1; i <= 50; i++) {
-		for(int j = 0; j < 12; j++) {
-			if(j == 0) {
-				printf("%d ", randStr[i][j]);
-			}
-			else printf("%c", randStr[i][j]);
-		}
-		printf("\n");
-	}
+    while(dsoc!=-1){
+        printf("Error while sending strings\n");
+        break;
+    }
+    
+    printer(strg);
+    
+    if(argc>1){
+        strncpy(buffer,"DOWN",sizeof("DOWN"));
+        write(dsoc,buffer,sizeof(buffer));
+        close(dsoc);
+        exit(EXIT_SUCCESS);
+    }
+    int count=0;
+    else{
+        int ind=1;
+        while(1){
+            printf("Sending Strings from %d to %d\n",ind,ind+4);
+            for(int i=ind;i<ind+5;i++){
+                count++;
+                connector=write(dsoc,strg[i],strlen(strg[i])+1);
+                if(connector==-1){
+                    count--;
+                    perror("Error :- couldn't write");
+                }
+            }
+            connector=read(dsoc,buffer,sizeof(buffer));
+            buffer[sizeof(buffer)-1]=0;
 
-	if(argc > 1) {
-		strncpy(buffer, "DOWN", sizeof("DOWN"));
-		write(data_socket, buffer, sizeof(buffer));
-		close(data_socket);
-		exit(EXIT_SUCCESS);
-	}
-	else {
-		int lastIdx = 1;
-		while(1) {
-			printf("sending Strings Indexed from %d to %d\n", lastIdx, lastIdx + 4);
-			for(int i = lastIdx; i < lastIdx + 5; i++) {
-				ret = write(data_socket, randStr[i], strlen(randStr[i]) + 1);
-				if(ret == -1) {
-					perror("couldn't write");
-				}
-			}
+            int finind=atoi(buffer);
+            printf("MAX ID received %s\n\n",buffer);
+            if(finind==50){
+                printf("Successfully sent all Strings\n");
+                clock_gettime(CLOCK_MONOTONIC,&end);
+                uint64_t difference=(e.tv_sec-s.tv_sec);
+                long double d,t;
+                d=(long double)(e.tv_nsec-s.tv_nsec)/1000000000;
+                t=(long long unsigned int)difference+d;
+                printf("Time elapsed %Lf\n",t);
+                strncpy(buffer,"DOWN",sizeof("DOWN"));
+                write(dsoc,buffer,sizeof(buffer));
+                close(dsoc);
+                exit(EXIT_SUCCESS);
+                break;
+            }
+            else{
+                ind=finalind+1;
+            }
+        }
+    }
 
-			//Reading Acknowledgements
-			ret = read(data_socket, buffer, sizeof(buffer));
-			if(ret == -1) {
-				perror("read");
-				exit(EXIT_FAILURE);
-			}
-
-			buffer[sizeof(buffer) - 1] = 0;
-			int finalIdx = atoi(buffer);
-			
-			printf("MAX ID SENT BACK BY SERVER = %s\n\n", buffer);
-			if(finalIdx == 50) {
-				printf("Successfully sent all Strings\n");
-				strncpy(buffer, "DOWN", sizeof("DOWN"));
-				write(data_socket, buffer, sizeof(buffer));
-				close(data_socket);
-				exit(EXIT_SUCCESS);
-				break;
-			}
-			else {
-				lastIdx = finalIdx + 1;
-			}
-		}
-	}
 }
